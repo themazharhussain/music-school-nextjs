9d1ae6d56823435ffa6a272231d9e8b331e3de83
Mazher Hussain <mazherhussain926@gmail.com>
2025-07-31 10:37:46 +0500

    disable eslint during build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["assets.aceternity.com", "images.unsplash.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
